788f929e9d55ccef58aa3b91e156542670cedb07
Jack Dippel <152130359+SkyVenture112@users.noreply.github.com>
2024-10-15 21:14:07 -0700

    Edited all files

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let message = "Hello.";
+
+
+alert(message);
+
+let admin;
+let name = "John";
+admin = name;
+alert(admin);
+
+let planetName = "Earth";
+const numberOfDays = 7;
+
+let result = prompt("Please enter an answer.", "");
+
+let isTrue = confirm("Is this true?");
+
+alert(isTrue);
+
+let cookie = {
+    flavor: "chocolate",
+    price: "$30"
+}
