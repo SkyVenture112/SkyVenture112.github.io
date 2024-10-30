b47d64adc9be3cf30148ee389578746cfd0a9241
SkyVenture112 <skyventure112@gmail.com>
2024-10-29 18:16:45 -0700

    Fixed navigation bar

--- script.js
+++ script.js
@@ -2,22 +2,21 @@
 
 let message = "Hello.";
 
-
-alert(message);
+// alert(message);
 
 let admin;
 let name = "John";
 admin = name;
-alert(admin);
+// alert(admin);
 
 let planetName = "Earth";
 const numberOfDays = 7;
 
-let result = prompt("Please enter an answer.", "");
+// let result = prompt("Please enter an answer.", "");
 
-let isTrue = confirm("Is this true?");
+// let isTrue = confirm("Is this true?");
 
-alert(isTrue);
+// alert(isTrue);
 
 let cookie = {
     flavor: "chocolate",
