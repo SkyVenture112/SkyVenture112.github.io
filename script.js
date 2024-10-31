fa4675cb2dc566a59fe3626ccb93386bfd54bc18
SkyVenture112 <152130359+SkyVenture112@users.noreply.github.com>
2024-10-30 21:25:16 -0700

    Fixed cursor effect

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ let cookie = {
     flavor: "chocolate",
     price: "$30"
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const typewriter = document.querySelector(".typewriter h1");
+    
+    // Add event listener for when the typing animation ends
+    typewriter.addEventListener("animationend", (event) => {
+      if (event.animationName === "typing") {
+        // Remove the blinking cursor by removing the border-right
+        typewriter.style.borderRight = "none";
+      }
+    });
+  });
+  
