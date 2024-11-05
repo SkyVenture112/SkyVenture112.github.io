a480fb8490be949d943f3fce18c300fc11c6309d
SkyVenture112 <152130359+SkyVenture112@users.noreply.github.com>
2024-11-05 15:32:51 -0800

    Revert

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 "use strict";
 
+let message = "Hello.";
+
+// alert(message);
+
+let admin;
+let name = "John";
+admin = name;
+// alert(admin);
+
+let planetName = "Earth";
+const numberOfDays = 7;
+
+// let result = prompt("Please enter an answer.", "");
+
+// let isTrue = confirm("Is this true?");
+
+// alert(isTrue);
+
+let cookie = {
+    flavor: "chocolate",
+    price: "$30"
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const typewriter = document.querySelector(".typewriter h1");
     
