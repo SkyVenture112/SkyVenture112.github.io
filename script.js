f4da9f076461f28fc2d8127187d703dd1dd00519
SkyVenture112 <152130359+SkyVenture112@users.noreply.github.com>
2024-11-13 20:36:56 -0800

    Edited readability on mobile

--- script.js
+++ script.js
@@ -3,10 +3,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const typewriter = document.querySelector(".typewriter h1");
     
-    // Add event listener for when the typing animation ends
+
     typewriter.addEventListener("animationend", (event) => {
       if (event.animationName === "typing") {
-        // Remove the blinking cursor by removing the border-right
         typewriter.style.borderRight = "none";
       }
     });
